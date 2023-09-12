3ed9e0599102b4be3ccd50eb047242fe2df3de04
sammykingx <hellosammy@sammykingx.com.ng>
2023-09-12 02:22:49 +0000

    app entry point

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,1 +1,15 @@
 # the application entry point
+from fastapi import FastAPI
+from models.db_engine import engine
+from models import db_schema
+from routes import users, messages
+
+app = FastAPI()
+
+# register routes
+app.include_router(users.router)
+app.include_router(messages.router)
+
+@app.get("/")
+def index():
+    return {"name": "sammykingx"}
