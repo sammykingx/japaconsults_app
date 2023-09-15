6911d6f901c2d0dcfbfac81935b70053376a6a32
sammykingx <hellosammy@sammykingx.com.ng>
2023-09-15 19:03:48 +0000

    [fixed] resolved sessionlocal errors

--- src/backend/models/db_engine.py
+++ src/backend/models/db_engine.py
@@ -15,7 +15,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = sessionlocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
