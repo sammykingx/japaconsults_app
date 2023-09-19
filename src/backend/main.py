fa11a8fbf73fea9c2709af572e2ac0f507170773
sammykingx <hellosammy@sammykingx.com.ng>
2023-09-19 22:21:56 +0000

    updated application entry point

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from models.db_engine import Base, engine
 from models import db_models
+from auth import user_login
 from routes import drafts, users, messages
 
 
@@ -11,6 +12,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # register routes
+app.include_router(user_login.router)
 app.include_router(drafts.router)
 app.include_router(users.router)
 app.include_router(messages.router)
@@ -19,9 +21,3 @@ app.include_router(messages.router)
 @app.get("/", tags=["Index"])
 def index():
     return {"status": "connection succesfull"}
-
-
-#if __name__ == "__main__":
-#    import uvicorn
-#
-#    uvicorn.run(app, host="0.0.0.0", port=5000)
